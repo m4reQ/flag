00fbd9633f762572ea521125a39a4ba94cc36ffc
m4reQ <m4reQ@users.noreply.github.com>
2021-11-20 16:56:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# PyFlag
+
+PyFlag is a Python library that helps with dealing with
+command line flag arguments parsing, through simple to use
+interface. Its architecture is based on the [flag](https://pkg.go.dev/flag) package
+available in GO langage. It doesn't require any additional dependencies.
+
+## Contribution
+If you wanna support development feel free to add an issue or feature proposal via 
+[flag/issues](github.com/m4reQ/flag/issues).
---
+++ flag/__init__.py
@@ -0,0 +1,227 @@
+from abc import ABC
+from typing import Any, Dict, SupportsFloat, SupportsInt, TypeVar, Type
+import re
+import sys
+import os
+
+__all__ = ['IntFlag', 'StrFlag', 'BoolFlag', 'FloatFlag', 'parse', 'print_defaults']
+
+FlagType = TypeVar('FlagType', str, int, bool, float)
+
+class _Flag(ABC):
+    @classmethod
+    def _check_against(cls, value: FlagType, _type: Type):
+        if not isinstance(value, _type):
+            _write_exc(f'Invalid type for flag of type {_type}: {type(value).__name__}')
+        
+    @classmethod
+    def _get_arg_name(cls, desc: str) -> str:
+        matches = re.findall(r"'(.*?)'", desc)
+        if not matches:
+            return ''
+        
+        if len(matches) > 1:
+            _write_exc(f'Argument names conflict in "{desc}", between: {", ".join(matches)}')
+        
+        return matches[0].replace("'", '')
+        
+    def __init__(self, name: str, default: FlagType, flag_type: Type, desc: str, mandatory: bool):
+        _Flag._check_against(default, flag_type)
+
+        self.name: str = name
+        self.default: FlagType = default
+        self.desc: str = desc
+        self.mandatory: bool = mandatory
+        self.type: Type = flag_type
+        self.arg_name: str = _Flag._get_arg_name(self.desc)
+        self._value: FlagType = self.default
+
+        if self.arg_name:
+            self.desc = self.desc.replace('\'', '')
+
+        _register_flag(self)
+    
+    def description(self) -> None:
+        if isinstance(self, BoolFlag):
+            print(f'--{self.name}')
+        else:
+            print(f'-{self.name} {self.arg_name}')
+        
+        print(f'    {self.desc}{" (mandatory)" if self.mandatory else ""}')
+    
+    @property
+    def is_default(self) -> bool:
+        return self.value == self.default
+    
+    @property
+    def value(self) -> FlagType:
+        return self._value
+    
+    @value.setter
+    def value(self, value: Any) -> None:
+        try:
+            conv = self.type(value)
+        except ValueError:
+            _write_exc(f'Cannot convert value "{value}" for {type(self).__name__} "{self.name}". (expected type: {self.type.__name__})')
+        
+        self._value = conv
+    
+    def __repr__(self) -> str:
+        return str(self)
+    
+    def __bool__(self) -> bool:
+        return bool(self.value)
+    
+    def __str__(self) -> str:
+        return str(self.value)
+    
+class IntFlag(_Flag, SupportsInt):
+    def __init__(self, name: str, default: int, desc: str, *, mandatory: bool=False):
+        super().__init__(name, default, int, desc, mandatory)
+    
+    def __int__(self) -> int:
+        return self.value
+    
+    def __eq__(self, o: SupportsInt) -> bool:
+        return self.value == int(o)
+    
+    def __ne__(self, o: SupportsInt) -> bool:
+        return self.value != int(o)
+    
+    def __lt__(self, o: SupportsInt) -> bool:
+        return self.value < int(o)
+
+    def __le__(self, o: SupportsInt) -> bool:
+        return self.value <= int(o)
+    
+    def __gt__(self, o: SupportsInt) -> bool:
+        return self.value > int(o)
+    
+    def __ge__(self, o: SupportsInt) -> bool:
+        return self.value >= int(o)
+
+class FloatFlag(_Flag, SupportsFloat):
+    def __init__(self, name: str, default: float, desc: str, *, mandatory: bool=False):
+        super().__init__(name, default, float, desc, mandatory)
+    
+    def __float__(self) -> float:
+        return self.value
+    
+    def __eq__(self, o: SupportsFloat) -> bool:
+        return self.value == float(o)
+    
+    def __ne__(self, o: SupportsFloat) -> bool:
+        return self.value != float(o)
+    
+    def __lt__(self, o: SupportsFloat) -> bool:
+        return self.value < float(o)
+
+    def __le__(self, o: SupportsFloat) -> bool:
+        return self.value <= float(o)
+    
+    def __gt__(self, o: SupportsFloat) -> bool:
+        return self.value > float(o)
+    
+    def __ge__(self, o: SupportsFloat) -> bool:
+        return self.value >= float(o)
+    
+class StrFlag(_Flag):
+    def __init__(self, name: str, default: str, desc: str, *, mandatory: bool=False):
+        super().__init__(name, default, str, desc, mandatory)
+    
+    def __eq__(self, o: object) -> bool:
+        return self.value == str(o)
+    
+    def __ne__(self, o: object) -> bool:
+        return self.value != str(o)
+
+class BoolFlag(_Flag):
+    def __init__(self, name: str, desc: str):
+        super().__init__(name, False, bool, desc, False)
+    
+    def __eq__(self, o: object) -> bool:
+        return self.value == bool(o)
+    
+    def __ne__(self, o: object) -> bool:
+        return self.value != bool(o)
+
+_flags_registered: Dict[str, _Flag] = {}
+_unsatisfied_mandatory: Dict[str, _Flag] = {}
+
+def _write_exc(exception: str) -> None:
+    sys.stderr.write(f'Error: {exception}\n')
+    print_defaults()
+    exit(1)
+
+def _register_flag(flag: _Flag) -> None:
+    if flag.name in _flags_registered:
+        _write_exc(f'_Flag already registered: {flag.name}.')
+    
+    _flags_registered[flag.name] = flag
+
+    if flag.mandatory:
+        _unsatisfied_mandatory[flag.name] = flag
+
+def print_defaults() -> None:
+    '''
+    Prints all flags usage informations.
+    '''
+
+    print(f'Usage of {os.path.basename(sys.argv[0])}:')
+
+    for flag in _flags_registered.values():
+        flag.description()
+
+def parse() -> None:
+    '''
+    Parses given command line arguments and puts values
+    into corresponding flags.
+    '''
+    
+    argv = sys.argv.copy()
+
+    argv.pop(0)
+
+    flags_provided = []
+
+    while len(argv) != 0:
+        arg = argv.pop(0)
+        real_name = ''
+        value = None
+        
+        if '=' in arg:
+            # support -name=value style flags
+            real_name, value = arg.split('=')
+        elif arg.startswith('--'):
+            # support --name style boolean flags
+            real_name = arg
+            value = True
+        elif arg.startswith('-'):
+            # support -name value style flags
+            real_name = arg
+            value = argv.pop(0)
+        else:
+            _write_exc(f'Invalid flag format: {arg}.')
+        
+        name = real_name.replace('-', '')
+
+        if name not in _flags_registered:
+            _write_exc(f'Unknown flag: {name}.')
+        
+        matched_flag = _flags_registered[name]
+
+        if isinstance(matched_flag, BoolFlag) and not real_name.startswith('--'):
+            if '=' in arg:
+                hint = '-flag_name=value'
+            else:
+                hint = '-flag_name value'
+            _write_exc(f'Boolean flags have to be specified as "--flag_name" not "{hint}".')
+
+        matched_flag.value = value
+        flags_provided.append(name)
+
+    for flag_name in flags_provided:
+        _unsatisfied_mandatory.pop(flag_name, None)
+    
+    if len(_unsatisfied_mandatory) != 0:
+        _write_exc(f'Mandatory flag "{list(_unsatisfied_mandatory.values())[0].name}" not provided.')
